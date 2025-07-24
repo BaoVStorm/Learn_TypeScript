1278dea6e7c2f78073eab3fd5072cfaa8c97811c
VStorm <tranvubao2004@gmail.com>
2025-07-24 18:53:44 +0700

    finish note

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,11 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Card from './components/Card';
 
+enum Degree {
+    A1,
+    A2,
+    A3,
+    B1,
+    B2,
+    B3,
+}
+
+interface People {
+    name: string;
+    age: number;
+    male?: boolean;
+}
+
+interface Employee extends People {
+    degree: Degree;
+}
+
+type Worker = People & {
+    abc: number;
+};
+
 function App() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number>(0);
+    const [people, setPeople] = useState<People>();
+
+    useEffect(() => {
+        setPeople({
+            name: 'VStorm',
+            age: 15,
+        });
+    }, []);
+
+    // console.log(people);
+
+    const employee: Employee = {
+        name: 'VStorm',
+        age: 18,
+        degree: Degree.A1,
+    };
+
+    console.log(employee);
+
+    const handleClick = (number1: number, number2: number): number => {
+        return number1 + number2;
+    };
 
     return (
         <>
@@ -14,7 +59,11 @@ function App() {
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
                 <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
                 </a>
             </div>
             <h1>Vite + React</h1>
@@ -26,8 +75,11 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to test HMR
                   </p>
                 </div> */}
-            <Card count={count} setCount={setCount} />
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <Card count={count} setCount={setCount} onClick={handleClick} />
+
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
         </>
     );
 }
--- code/src/components/Card.tsx
+++ code/src/components/Card.tsx
@@ -1,8 +1,27 @@
-function Card({ count, setCount }: { count: number; setCount: React.Dispatch<React.SetStateAction<number>> }) {
+import React from 'react';
+
+// type Props = {
+//     count: number;
+//     setCount: React.Dispatch<React.SetStateAction<number>>;
+// };
+
+interface IProps {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>;
+    onClick: (n1: number, n2: number) => number;
+}
+
+function Card({ count, setCount, onClick }: IProps) {
     return (
         <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-            <p>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p
+                onClick={() => {
+                    console.log(onClick(23, 23));
+                }}
+            >
                 Edit <code>src/App.tsx</code> and save to test HMR
             </p>
         </div>
