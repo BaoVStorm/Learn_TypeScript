3e68ee640af89afe144d62b57d663df31843b40f
VStorm <tranvubao2004@gmail.com>
2025-07-24 17:36:41 +0700

    basic typesript

---
+++ code/src/components/Card.tsx
@@ -0,0 +1,12 @@
+function Card({ count, setCount }: { count: number; setCount: React.Dispatch<React.SetStateAction<number>> }) {
+    return (
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+            <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+        </div>
+    );
+}
+
+export default Card;
